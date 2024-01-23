5f84c903fc841c25ecb573ff383707cef944558a
Gabriel Sato <gabriel.sato@empiricus.com.br>
2024-01-23 09:01:10 -0300

    Relembrando conceitos básicos de TS

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # typescript-studies
 Repositório para estudos de Typescript.
+
+## Get Started
+
+1. Instalar o TS [npm i -g]
+2. Inicializar o TS no projeto [tsc init]
+3. Observar mudanças nos arquivos TS [tsc -w]
---
+++ basic-concepts.js
@@ -0,0 +1,33 @@
+"use strict";
+// TYPE ANNOTATIONS - indicar o tipo de uma variável ou expressão
+const product = 'Movie';
+const movie = {
+    title: 'Lord of the Rings',
+    budget: 93000000,
+};
+// INFERENCE - o TypeScript identifica o tipo da variável em sua declaração
+const director = 'Peter Jackson';
+console.log(typeof director); // string
+/* OBS: para declarações de variáveis, em boa parte dos casos, não é necessário
+anotar o tipo. Sempre anotar ao lidar com parâmetros e retornos de funções. */
+function calculateBoxOffice(ticketPrice, soldTickets) {
+    return ticketPrice * soldTickets;
+}
+calculateBoxOffice(15, 1000);
+// UNION TYPES - descreve um valor que pode ter vários tipos (evitar usar no retorno)
+let releaseDate;
+function isOldMovie(releaseYear) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear -
+        (typeof releaseYear === 'number' ? releaseYear : Number(releaseYear));
+    return age >= 20;
+}
+function fillMovieData(data) {
+    return `${data.title}, lançado em ${data.release_year}, dirigido por ${data.director}${data.restricted ? ' (+18)' : ''}`;
+}
+console.log(fillMovieData({
+    title: 'Halloween',
+    release_year: 1978,
+    director: 'John Carpenter',
+    restricted: true,
+}));
