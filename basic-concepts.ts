2d466795f6015498d338034956d2e8d3dba603b3
Gabriel Sato <gabriel.sato@empiricus.com.br>
2024-01-23 16:07:09 -0300

    Adiciona explicação sobre interfaces e types

--- basic-concepts.ts
+++ basic-concepts.ts
@@ -11,7 +11,7 @@ const movie: {
 
 // INFERENCE - o TypeScript identifica o tipo da variável em sua declaração
 const director = 'Peter Jackson';
-console.log(typeof director); // string
+typeof director; // string
 
 /* OBS: para declarações de variáveis, em boa parte dos casos, não é necessário
 anotar o tipo. Sempre anotar ao lidar com parâmetros e retornos de funções. */
@@ -35,23 +35,48 @@ function isOldMovie(releaseYear: string | number): boolean {
 	return age >= 20;
 }
 
-// OBJECTS and TYPE
-type Movie = {
-  title: string;
+/* OBJECTS, TYPE and INTERFACE
+
+Em TypeScript, tanto "types" quanto "interfaces" são utilizados para definir tipos de dados,
+mas há diferenças sutis entre eles.
+
+Interfaces:
+	- As interfaces são mais frequentemente utilizadas para definir contratos para objetos.
+		Elas descrevem a forma que um objeto deve ter.
+	- Permitem a herança, o que significa que você pode estender uma interface a partir de outra.
+	- Podem ser usadas para declarar tipos de objetos, funções, classes e até mesmo arrays.
+
+	Use interfaces quando estiver trabalhando principalmente com objetos e precisar de recursos
+	como herança de tipos e extensibilidade para criar contratos claros para estruturas de dados.
+
+Types:
+  - Os tipos (types) são mais versáteis e podem ser usados para criar tipos de união, tipos literais,
+		tipos condicionais, e outras construções mais avançadas.
+  - Permitem criar tipos que não se limitam apenas a objetos, podendo ser usados para definir
+		tipos para qualquer valor, inclusive primitivos.
+
+	Use tipos quando precisar de recursos mais avançados ou quando estiver trabalhando com tipos que
+	não são exclusivamente objetos. Eles são mais adequados para situações em que você precisa criar
+	tipos complexos ou uniões de tipos.
+*/
+type Director = string;
+
+interface InterfaceMovie {
+	title: string;
 	release_year: number;
-	director: string;
+	director: Director;
 	restricted: boolean;
 };
 
-function fillMovieData(data: Movie) {
+function fillMovieData(data: InterfaceMovie) {
 	return `${data.title}, lançado em ${data.release_year}, dirigido por ${
 		data.director
 	}${data.restricted ? ' (+18)' : ''}`;
 }
 
-console.log(fillMovieData({
+fillMovieData({
   title: 'Halloween',
   release_year: 1978,
   director: 'John Carpenter',
   restricted: true,
-}));
+});
