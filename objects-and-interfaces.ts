240e89e74910560cbfb7ab7eab9d3d993d07d296
Gabriel <gabsato02@gmail.com>
2024-01-25 17:55:02 -0300

    Primeira parte dos estudos sobre interfaces

---
+++ objects-and-interfaces.ts
@@ -0,0 +1,51 @@
+// DEFINIÇÃO DE CLASSE
+class Product {
+  name: string;
+  price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+
+  realPrice() {
+    return `R$ ${this.price}`;
+  }
+}
+
+// INSTÂNCIA
+class Book extends Product {
+  author: string;
+
+  constructor(name: string, price: number, author: string) {
+    super(name, price); // acessando propriedades da herança da classe
+    this.author = author
+  }
+}
+
+const book = new Book('The Lord of the Rings', 80, 'J. R. R. Tolkien');
+
+console.log(book instanceof Book); // true - Verificando se o objeto é uma instância de uma classe
+console.log(book instanceof Product); // true - Pois a classe Book herda de Produto
+
+/* DOM
+
+Os elementos do DOM, possuem uma herança de várias classes:
+
+EventTarget > Node > Element > HTMLElement > HTMLAnchorElement (classe específica)
+
+Ao verificar uma dessas classes com o instaceof, é possível acessar métodos e propriedades dela
+
+*/
+
+const a = document.querySelectorAll('a');
+console.log(a instanceof NodeList); // true
+
+// EVENTS
+const button = document.querySelector('button');
+
+function handleClick(event: Event) {
+  console.log(event);
+}
+
+button?.addEventListener('click', handleClick);
